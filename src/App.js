cd5c757fbd514b8983cffd45a43debfcb51b5bbf
Chross <matia.medinag@alumnos.duoc.cl>
2019-06-12 02:38:59 -0400

    Jumbotron added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Contact } from './components/Contact';
-import { NoMatch } from './components/NoMatch';
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
+import { NoMatch } from './NoMatch';
 import { Layout } from './Layout';
-import { NavigationBar } from './components/Navigation'
+import { NavigationBar } from './components/Navigation';
+import { Jumbotron } from './components/Jumbotron';
 
 function App() {
   return (
     <React.Fragment>
-      <NavigationBar />
       <Layout>
         <Router>
+          <NavigationBar />
+          <Jumbotron />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
